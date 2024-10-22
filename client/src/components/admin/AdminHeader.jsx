9a036f823145b67aa083fae84fc9ef4f621b7738
Sandip-Chavda <chavdas766@gmail.com>
2024-10-22 12:44:57 +0530

    Admin Logout Added

--- client/src/components/admin/AdminHeader.jsx
+++ client/src/components/admin/AdminHeader.jsx
@@ -1,8 +1,16 @@
+import { logoutUser } from "@/redux/slices/authSlice";
 import { Button } from "../ui/button";
 import { AiOutlineMenu } from "react-icons/ai";
 import { MdLogout } from "react-icons/md";
+import { useDispatch } from "react-redux";
 
 const AdminHeader = ({ setOpen }) => {
+  const dispatch = useDispatch();
+
+  function handleLogout() {
+    dispatch(logoutUser());
+  }
+
   return (
     <header className="flex items-center justify-between px-4 py-3 bg-background border-b">
       <Button
@@ -16,6 +24,7 @@ const AdminHeader = ({ setOpen }) => {
       </Button>
       <div className="flex flex-1 justify-end">
         <Button
+          onClick={handleLogout}
           className="inline-flex items-center gap-1.5 bg-white border-red-500 text-red-500 hover:bg-red-500 hover:text-white transition-all duration-200"
           variant="outline"
         >
